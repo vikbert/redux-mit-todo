1236b35bab4cac5d359f4b356b9825e5bfb193bc
Xun Zhou <segentor@gmail.com>
2019-09-28 00:04:39 +0200

    updates

--- src/http/todosClient.js
+++ src/http/todosClient.js
@@ -2,19 +2,21 @@ const URL_TODOS = 'https://vikbert-software.de/api/todos.php';
 export const saveTodos = (todos = []) => {
     fetch(URL_TODOS, {
         method: 'POST',
+        mode: 'no-cors',
         body: JSON.stringify(todos),
     }).catch((error) => {
-        console.error('POST data failed: ', error);
+        console.error('POST todos failed: ', error);
     });
 };
 
 export const loadTodos = () => {
     fetch(URL_TODOS, {
         method: 'GET',
+        mode: 'no-cors',
     }).then((response) => {
         console.log(response);
         return response;
     }).catch((error) => {
-        console.error('POST data failed: ', error);
+        console.error('Loading todos failed: ', error);
     });
 };
